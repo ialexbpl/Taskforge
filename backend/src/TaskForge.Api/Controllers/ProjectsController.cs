47e39e6c3a1524facecfe18c92e0833e5ca06fdb
ialexbpl <spartan03311@gmail.com>
2025-08-11 19:18:00 +0200

    Add project files.

---
+++ backend/src/TaskForge.Api/Controllers/ProjectsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskForge.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+    }
+}
