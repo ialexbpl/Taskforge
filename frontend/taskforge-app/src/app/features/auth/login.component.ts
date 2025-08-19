3b9f6b72129f1ccb53182fd406b4f71d02fb2a8a
ialexbpl <spartan03311@gmail.com>
2025-08-19 13:13:29 +0200

    Features => login

--- frontend/taskforge-app/src/app/features/auth/login.component.ts
+++ frontend/taskforge-app/src/app/features/auth/login.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { AuthService } from '../../core/api/auth.service';
 
 @Component({
+ standalone: true,
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email=''; password=''; error=''; busy=false;
+  name=''; regEmail=''; regPass='';
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  login(){
+    this.busy=true; this.error='';
+    this.auth.login(this.email, this.password).subscribe({
+      next: r => { localStorage.setItem('access_token', r.accessToken); this.router.navigateByUrl('/projects'); },
+      error: _ => { this.error='Invalid credentials'; this.busy=false; }
+    });
+  }
+  register(){
+    this.auth.register(this.regEmail, this.regPass, this.name).subscribe({
+      next: _ => { this.email=this.regEmail; this.password=this.regPass; this.login(); },
+      error: e => this.error = e?.error ?? 'Registration failed'
+    });
+  }
 }
