4cf90c93fec0ea8eb39b8daf2d8f7ffd6b52750c
ialexbpl <spartan03311@gmail.com>
2025-08-19 13:15:00 +0200

    Features => listing

--- frontend/taskforge-app/src/app/features/projects/list.component.ts
+++ frontend/taskforge-app/src/app/features/projects/list.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+import { Router } from '@angular/router';
+import { ProjectsService } from '../../core/api/projects.service';
 
 @Component({
-  selector: 'app-list',
-  imports: [],
+  standalone: true,
+  selector: 'app-projects-list',
+  imports: [FormsModule, NgFor],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
-
+projects:any[]=[]; name=''; key='';
+  constructor(private projectsApi: ProjectsService, private router: Router) {}
+  ngOnInit(){ this.projectsApi.list().subscribe(x=>this.projects=x); }
+  create(){ this.projectsApi.create(this.name, this.key).subscribe(x=>{ this.projects.push(x); this.name=''; this.key=''; }); }
+  openBoard(p:any){ this.router.navigate(['/board', p.id]); }
 }
